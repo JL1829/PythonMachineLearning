6cae46d2eeb77fedd2be5905a731b26ba8b5299e
JL1829 <johnnylou89@icloud.com>
2020-12-03 14:29:59 +0800

    add doc test, example docstring

--- utilis/distance.py
+++ utilis/distance.py
@@ -8,7 +8,15 @@ def euclidean(x, y):
     :param x: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :param y: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :return:  float, the L2 distance between 'x' vector and 'y' vector
+
+    ----------
+    Example:
+    >>> x = np.array([1, 2, 3, 4, 5])
+    >>> y = np.array([6, 7, 8, 9, 10])
+    >>> euclidean(x, y)
+    11.180339887498949
     """
+
     return np.sqrt(np.sum((x - y) ** 2))
 
 
@@ -19,6 +27,13 @@ def manhattan(x, y):
     :param x: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :param y: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :return:  float, the L1 distance between 'x' vector and 'y' vector
+
+    ----------
+    Example:
+    >>> x = np.array([1, 2, 3, 4, 5])
+    >>> y = np.array([6, 7, 8, 9, 10])
+    >>> manhattan(x, y)
+    25
     """
     return np.sum(np.abs(x - y))
 
@@ -30,6 +45,13 @@ def chebyshev(x, y):
     :param x: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :param y: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :return:  float, the Chebyshev distance between 'x' vector and 'y' vector
+
+    ----------
+    Example:
+    >>> x = np.array([1, 2, 3, 4, 5])
+    >>> y = np.array([6, 7, 8, 9, 10])
+    >>> chebyshev(x, y)
+    5
     """
     return np.max(np.abs(x - y))
 
@@ -41,5 +63,12 @@ def hamming(x, y):
     :param x: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :param y: {array-like} 'ndarray <numpy ndarray>' of shape: [vector_length, ]
     :return:  float, the Hamming distance.
+
+    ----------
+    Example:
+    >>> x = np.array([1, 2, 3, 4, 5])
+    >>> y = np.array([6, 7, 8, 9, 10])
+    >>> hamming(x, y)
+    1.0
     """
     return np.sum(x != y) / len(x)
